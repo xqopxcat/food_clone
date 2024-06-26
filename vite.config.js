7b864880044571e8360f0b2d4c3b930e3b0ce2a8
Steve Lee 李孟儒 <steve.lee@TWTPMAC10004148.local>
2024-06-26 12:06:15 +0800

    manifest.json

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,36 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            },
+            manifest: {
+                "name": "Food Clone",
+                "short_name": "Food",
+                "theme_color": "#000000",
+                "icons": [
+                  {
+                    "src": "assets/img/icons192.png",
+                    "type": "image/png",
+                    "sizes": "192x192"
+                  },
+                  {
+                    "src": "assets/img/icons512.png",
+                    "type": "image/png",
+                    "sizes": "512x512"
+                  }
+                ],
+                "start_url": "./?utm_source=pwa_app",
+                "id": "/?utm_source=web_app_manifest",
+                "background_color": "#FFFFFF",
+                "display": "fullscreen",
+                "orientation": "portrait",
+                "prefer_related_applications": false
+            }
+        })
+    ],
 })
