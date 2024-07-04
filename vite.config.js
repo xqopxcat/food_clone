2a4828df4125d195aa1ec10f1b1fa2a71112e806
Steve Lee 李孟儒 <steve.lee@TWTPMAC10004148.local>
2024-07-04 10:51:29 +0800

    test for apply pay

--- vite.config.js
+++ vite.config.js
@@ -6,34 +6,22 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-            registerType: 'autoUpdate',
-            devOptions: {
-                enabled: true
-            },
-            manifest: {
-                "name": "Food Clone",
-                "short_name": "Food",
-                "theme_color": "#000000",
-                "icons": [
-                  {
-                    "src": "assets/img/icons192.png",
-                    "type": "image/png",
-                    "sizes": "192x192"
-                  },
-                  {
-                    "src": "assets/img/icons512.png",
-                    "type": "image/png",
-                    "sizes": "512x512"
-                  }
-                ],
-                "start_url": "./?utm_source=pwa_app",
-                "id": "/?utm_source=web_app_manifest",
-                "background_color": "#FFFFFF",
-                "display": "fullscreen",
-                "orientation": "portrait",
-                "prefer_related_applications": false
-            }
-        })
+    // VitePWA({
+    //         registerType: 'autoUpdate',
+    //         devOptions: {
+    //             enabled: true
+    //         },
+    //         manifest: {
+    //             "name": "Food Clone",
+    //             "short_name": "Food",
+    //             "theme_color": "#000000",
+    //             "start_url": "./?utm_source=pwa_app",
+    //             "id": "/?utm_source=web_app_manifest",
+    //             "background_color": "#FFFFFF",
+    //             "display": "fullscreen",
+    //             "orientation": "portrait",
+    //             "prefer_related_applications": false
+    //         }
+    //     })
     ],
 })
