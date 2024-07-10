07442889da86b4b256775cfb53abb16f161e4239
Steve Lee 李孟儒 <steve.lee@TWTPMAC10004148.local>
2024-07-10 18:01:58 +0800

    active rotate navigator item

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,21 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-
-
-
-
-
-
-
-
-
-
-const NavigationItem = ({ title, icon }) => {
+const NavigationItem = ({ title, icon, active, onClick }) => {
     return (
-        <li className="flex items-center justify-center flex-col min-w-[72px]">
+        <li onClick={ () => {
+            if (active) return onClick()
+            return onClick(title)
+        } } className="flex items-center justify-center flex-col min-w-[72px]">
             <div className="mb-[4px]">
                 <img
-                    className="w-16 h-16"
+                    className={`w-16 h-16 smooth-transition rounded-full ${ active ? 'rotate-[20deg] bg-[#d3efda]' : ''}`}
                     alt=""
                     role="presentation"
                     src={ icon }
@@ -32,12 +25,13 @@ const NavigationItem = ({ title, icon }) => {
 }
 
 const NavigationBar = ({ items, children }) => {
+    const [activeType, setActiveType] = useState();
     return (
         <nav className="p-[4px_0px_2px_8px] no-scrollbar overflow-y-auto">
             <ul className="flex flex-row gap-1">
                 {
                     children ?? items.map(({ title, icon}) => {
-                        return <NavigationItem title={ title } icon={ icon } />
+                        return <NavigationItem title={ title } icon={ icon } active={ title === activeType } onClick={ setActiveType } />
                     })
                 }
             </ul>
